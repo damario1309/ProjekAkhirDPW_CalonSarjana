2c0452621a137b3907d157d59762c82513de4610
damario1309 <80190661+damario1309@users.noreply.github.com>
2021-06-08 23:43:16 +0700

    Update koneksi.php

--- function/koneksi.php
+++ function/koneksi.php
@@ -1,9 +1,9 @@
 <?php
 
-	$server = "localhost";
-	$username = "root";
-	$password = "";
-	$database = "db_dpw";
+	$server = "remotemysql.com";
+	$username = "kAi0LeQ6Iq";
+	$password = "m5NHqskQWd";
+	$database = "kAi0LeQ6Iq";
 	
 	$koneksi = mysqli_connect($server, $username, $password, $database) or die("Koneksi ke database gagal");
 	
